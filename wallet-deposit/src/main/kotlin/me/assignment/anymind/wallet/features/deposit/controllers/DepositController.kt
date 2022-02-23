89a6926ef5621ae45953808289d169cea5fba484
Nott - Nattaporn Chatmalairut <nattaporn.cha@everapp.io>
2022-02-23 22:30:05 +0700

    Add library validation, Refactoring deposit api should validate body request `datetime`, `amount`.

--- wallet-deposit/src/main/kotlin/me/assignment/anymind/wallet/features/deposit/controllers/DepositController.kt
+++ wallet-deposit/src/main/kotlin/me/assignment/anymind/wallet/features/deposit/controllers/DepositController.kt
@@ -1,18 +1,45 @@
 package me.assignment.anymind.wallet.features.deposit.controllers
 
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import java.util.*
+import javax.validation.Valid
+import javax.validation.constraints.*
 
 @RestController
 @RequestMapping("/api/wallet")
 class DepositController {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     @PostMapping("/deposit", produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
-    fun deposit(): ResponseEntity<String> {
-        return ResponseEntity.ok("""{ "code" : 0, "message" : "success"}""")
+    fun deposit(@Valid @RequestBody request: DepositRequest): ResponseEntity<DepositResponse> {
+        logger.info("Received request $request")
+        val response = DepositResponse(
+            code = 0,
+            message = "success"
+        )
+        return ResponseEntity.ok(response).also {
+            logger.info("Response $response")
+        }
     }
 }
+
+data class DepositRequest(
+    @field:NotNull
+    val datetime: ZonedDateTime,
+    @field:NotNull
+    @field:DecimalMin(value = "0.0", inclusive = false, message = "The decimal value can not be less than 0.0")
+    @field:DecimalMax(value = "9999999999.99999", inclusive = false, message = "The decimal value can not be more than 9999999999.99999")
+    val amount: BigDecimal
+)
+
+data class DepositResponse(
+    val code: Int,
+    val message: String
+)
