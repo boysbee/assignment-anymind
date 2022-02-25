4c55e17d8292d45735ace99df84884cf14a208fd
Nott - Nattaporn Chatmalairut <nattaporn.cha@everapp.io>
2022-02-25 18:57:30 +0700

    Add async on event listener.

--- wallet-deposit/src/main/kotlin/me/assignment/anymind/wallet/handlers/WalletTransactionCreateListener.kt
+++ wallet-deposit/src/main/kotlin/me/assignment/anymind/wallet/handlers/WalletTransactionCreateListener.kt
@@ -3,16 +3,19 @@ package me.assignment.anymind.wallet.handlers
 import me.assignment.anymind.wallet.events.TransactionEvent
 import me.assignment.anymind.wallet.services.WalletService
 import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import java.sql.Timestamp
 
-@Component
+@Configuration
 class WalletTransactionCreateListener(
     private val walletService: WalletService
 ) {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
+    @Async
     @EventListener(TransactionEvent::class)
     fun saveWallet(transactionEvent: TransactionEvent) {
         this.walletService.saveTransaction(
