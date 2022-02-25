95d024a2124e29aa7700c4bc2ca3c43cb212c862
Nott - Nattaporn Chatmalairut <nattaporn.cha@everapp.io>
2022-02-25 18:40:58 +0700

    Modify build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-echo `Start to build image wallet-deposit ...`
+echo "Start to build image wallet-deposit ..."
 cd wallet-deposit
 DEPOSIT_FOLDER=$(pwd)
-
-
 mvn spring-boot:build-image
+echo "End build build image at path : ${DEPOSIT_FOLDER} done."
 
-echo `End build $DEPOSIT_FOLDER done.` 
-echo `Start to build image wallet-history ...`
+echo "Start to build image wallet-history ..."
 cd ../wallet-history
-
 HISTORY_FOLDER=$(pwd)
 
-
 mvn spring-boot:build-image
-
-cd ..
-
-echo `End build $HISTORY_FOLDER done. `
+echo "End build image at path : ${HISTORY_FOLDER} done. "
+echo "End build all images."
