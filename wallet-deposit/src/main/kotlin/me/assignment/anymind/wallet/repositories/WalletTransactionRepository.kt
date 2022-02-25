02eb9bda3a68aa577a680003a0b1a9b1996b0631
Nott - Nattaporn Chatmalairut <nattaporn.cha@everapp.io>
2022-02-25 14:27:35 +0700

    Add function `findLastBalance`

--- wallet-deposit/src/main/kotlin/me/assignment/anymind/wallet/repositories/WalletTransactionRepository.kt
+++ wallet-deposit/src/main/kotlin/me/assignment/anymind/wallet/repositories/WalletTransactionRepository.kt
@@ -2,7 +2,7 @@ package me.assignment.anymind.wallet.repositories
 
 import me.assignment.anymind.wallet.entities.WalletTransactions
 import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 
-@Component
-interface WalletTransactionRepository : CrudRepository<WalletTransactions, String>
+@Service
+interface WalletTransactionRepository : CrudRepository<WalletTransactions, String> {}
